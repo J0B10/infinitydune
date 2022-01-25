207cdef3337aa8008524fbf6fb6e9d1aee3e55b8
Jonas <17405009+J0B10@users.noreply.github.com>
2022-01-25 18:37:57 +0100

    Build utillities based on jadc/modpack-manifest-generator <https://github.com/jadc/modpack-manifest-generator>

---
+++ build/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# check if all required files exist
+
+if [ ! -f overrides.txt ]; then
+    >&2 echo "missing overrides.txt"
+    exit 1
+fi
+
+if [ ! -f manifest-template.json ]; then
+    >&2 echo "missing manifest-template.json"
+    exit 1
+fi
+
+if [ ! -f mods.txt ]; then
+    >&2 echo "missing mods.txt"
+    exit 1
+fi
+
+# uses https://github.com/jadc/modpack-manifest-generator
+wget -nc "https://raw.githubusercontent.com/jadc/modpack-manifest-generator/1.2.1/generate.py"
+
+if [ ! -f generate.py ]; then
+    >&2 echo "missing generate.py"
+    exit 1
+fi
+
+# cleanup old files
+echo "cleanup out"
+rm -rf out
+
+# copy overrides
+
+echo "Copy overrides:"
+mkdir -p out/overrides
+while read f; do
+  cp -r "../$f" "out/overrides/$f"
+  echo "    copied $f"
+done <overrides.txt
+
+# generate manifest
+
+echo "Generating manifest:"
+python3 generate.py
+
+echo "Copy manifest"
+mv manifest.json out
+
+read -p "Press [Enter] key to continue..."
